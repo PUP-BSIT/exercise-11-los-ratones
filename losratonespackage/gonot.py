c16ab5484be8eb916f12e913f9c4a24dc704b2d9
gonot_jediduncan <jediduncan4@gmail.com>
2025-05-13 23:13:34 +0800

    add gonot module

---
+++ losratonespackage/gonot.py
@@ -0,0 +1,14 @@
+import random
+
+def movie():
+    name = input("What's your name?: ")
+    movie = input("What's your favorite movie?: ")
+
+    if movie.strip() == "":
+        print("Good bye")
+    else:
+        response = random.choice([
+            f"{name}, this movie is absolute cinema!",
+            f"{name}, this movie is trash"
+        ])
+        print(response)
